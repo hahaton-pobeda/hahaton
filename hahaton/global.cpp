46041193afcfa4e6a5971c3829113ab8a3560c9f
AlexUner <69047343+AlexUner@users.noreply.github.com>
2021-12-05 11:36:28 +0300

    hehe

--- hahaton/global.cpp
+++ hahaton/global.cpp
@@ -20,7 +20,7 @@ bool global::check(double* begin_pos, double* end_pos)
 bool global::ConnectToDB()
 {
     try
-    {      
+    {
         Connection = new pqxx::connection("dbname = db1 user = rouser password = Pass1234  host = rc1c-90mkmxvku0h105em.mdb.yandexcloud.net port = 6432 sslmode = require");;
         if (Connection->is_open())
         {
@@ -40,7 +40,7 @@ bool global::ConnectToDB()
     }
 }
 
-double global::distance_km(double lat1, double lon1, double lat2, double lon2) 
+double global::distance_km(double lat1, double lon1, double lat2, double lon2)
 {
     const double radiant = 0.01745329251;
     double a = 0.5 - cos((lat2 - lat1) * radiant) / 2. + cos(lat1 * radiant) * cos(lat2 * radiant) * (1. - cos((lon2 - lon1) * radiant)) / 2.;
@@ -89,17 +89,163 @@ void global::take_cars(std::string start_time, std::string end_time, double *beg
 
     pqxx::work Work{ *Connection };
 
-    pqxx::result Result = Work.exec("select grz, lat, lon, address, date from fvf where date between " + start_time + " and " + end_time + "\
+    pqxx::result Result = Work.exec("select grz, lat, lon, address, date from fvf where (date between " + start_time + " and " + end_time + "\
                                     and (lat between " + std::to_string(begin_pos[1]) + " and " + std::to_string(end_pos[1]) + "\
-                                    ) and (lon between " + std::to_string(begin_pos[0]) + " and " + std::to_string(end_pos[0]) + ")");
+                                    ) and (lon between " + std::to_string(begin_pos[0]) + " and " + std::to_string(end_pos[0]) + ")" + "\
+                                    ) ORDER BY grz, date"); // ЗДЕСЬ ИЗМЕНИЛИ
 
-    cars = new car[Result.size()];
+    std::vector <car> cars;
+
+    car car_temp;
+    cam cam_temp;
 
     for (int rownum = 0; rownum < Result.size(); ++rownum)
     {
-        pqxx::field const field = Result[rownum][0];
-        cars->grz = 
+        // Нужно пройтись по строчкам:
+        // если rownum == 0 , то просто cars[0].grz = Result[0][0]
+        // иначе (если rownum > 0) если Result[rownum][0] отличается от grz текущего автомобиля, то создаем новый автомобиль и присваиваем ему этот grz
+        // пока grz одинаковый и дата текущей и предыдущей строчек различная, создаем новую камеру в машине
+
+        pqxx::field field = Result[rownum][0];
+
+        if (rownum == 0) 
+        {
+            car_temp.grz = field.as<std::string>();
+            cars.push_back(car_temp);
+
+            field = Result[rownum][3];
+            cam_temp.address = field.as<std::string>();
+
+            //field = Result[rownum][4];
+            //cam_temp.date = field.as<tm>();
+
+            field = Result[rownum][2];
+            cam_temp.coords[0] = field.as<double>();
+
+            field = Result[rownum][1];
+            cam_temp.coords[1] = field.as<double>();
+
+            cars[0].camera.push_back(cam_temp);
+        }
+        else 
+        {
+            if (field.as<std::string>() == cars[cars.size()-1].grz) 
+            {
+                field = Result[rownum][3];
+                cam_temp.address = field.as<std::string>();
+
+                //field = Result[rownum][4];
+                //cam_temp.date = field.as<tm>();
+
+                field = Result[rownum][2];
+                cam_temp.coords[0] = field.as<double>();
+
+                field = Result[rownum][1];
+                cam_temp.coords[1] = field.as<double>();
+
+                cars[cars.size()-1].camera.push_back(cam_temp);
+
+                if (cars[cars.size()-1].camera.size() >= 2) 
+                {
+                    fill_matrix(take_direction(cars[cars.size() - 1].camera[cars[cars.size() - 1].camera.size() - 2],
+                        cars[cars.size() - 1].camera[cars[cars.size() - 1].camera.size() - 1]),
+                        matrix_directions);
+                }
+            }
+            else 
+            {
+                car_temp.grz = field.as<std::string>();
+                cars.push_back(car_temp);
+
+                field = Result[rownum][3];
+                cam_temp.address = field.as<std::string>();
+
+                //field = Result[rownum][4];
+                //cam_temp.date = field.as<tm>();
+
+                field = Result[rownum][2];
+                cam_temp.coords[0] = field.as<double>();
+
+                field = Result[rownum][1];
+                cam_temp.coords[1] = field.as<double>();
+
+                cars[cars.size()-1].camera.push_back(cam_temp);
+            }
+        }
     }
 
     std::cout << "Расчет трафика завершен!\n";
+}
+
+area* global::make_areas() {
+    // left_down -> right_up
+    //[x][y][[lon1][lat1][lon2][lat2]]
+
+    // эта функция должна вызваться до машин, но после поиска углов
+    // функция возвращает массив N*N структур area
+    // В каждом area отмечены левый нижний угол [0] и правый верхний угол [1] соответствующего сектора
+
+    double h_x = (right_up[0] - left_down[0]) / N;
+    double h_y = (right_up[1] - left_down[1]) / N;
+
+    areas = new area[N*N];
+
+    for (int i=0; i < N; i++) {
+        for (int j=0; j < N; j++) {
+            areas[i*N+j].coords[0][0] = left_down[0] + j*h_x;
+            areas[i*N+j].coords[0][1] = right_up[1] - (i+1)*h_y;
+
+            areas[i*N+j].coords[1][0] = left_down[0] + (j+1)*h_x;
+            areas[i*N+j].coords[1][1] = right_up[1] - i*h_y;
+        }
+    }
+
+    // cout << areas[0].coords[0][0] << " and " << left_down[0] << endl;
+    return areas;
+}
+
+int* global::take_direction(cam cam1, cam cam2) {
+    // Нужно пройтись по массиву машин, внутри которого
+    // camera[нач] , camera[кон]
+    // отсеиваем ненужные результаты: если начальная и конечная координаты находятся в одном секторе, то сразу возвращаем [-1, -1]
+    // иначе вернем два номера районов [нач. район, кон. район]
+    // areas - глобальный
+
+    int answer[2] = {-1, -1};
+
+    if (cam1.coords[0] == cam2.coords[0] && cam1.coords[1] == cam2.coords[1])
+        return answer;
+
+    for (int i=0; i < N; i++) 
+        for (int j=0; j < N; j++) 
+            if (cam1.coords[0]  > areas[i*N+j].coords[0][0] && cam1.coords[0] <= areas[i*N+j].coords[1][0] ) 
+                if ( cam1.coords[1] > areas[i*N+j].coords[0][1] && cam1.coords[0] <= areas[i*N+j].coords[1][1] ) 
+                {
+                    answer[0] = i*N+j;
+                    break;
+                }
+
+    for (int i=0; i < N; i++) 
+        for (int j=0; j < N; j++) 
+            if (cam2.coords[0] > areas[i*N+j].coords[0][0] && cam2.coords[0] <= areas[i*N+j].coords[1][0] ) 
+                if ( cam2.coords[1] > areas[i*N+j].coords[0][1] && cam2.coords[0] <= areas[i*N+j].coords[1][1] ) 
+                {
+                    answer[1] = i*N+j;
+                    break;
+                }
+
+    return answer;
+}
+
+void global::fill_matrix(int* directions, int matrix[][N]) {
+    // В самом-самом начале матрица должна быть заполнена нулями, по главной диагонали -1
+    // car.directions[0] - откуда
+    // car.directions[1] - куда
+    // Перемещаемся из строки в столбец
+    // если машина не уехала из сектора, то из take_directions вернется direcions[0] == directions[1]
+
+    if (directions[0] == -1 || directions[1] == -1) {
+        return;
+    }
+    matrix[directions[0]][directions[1]] += 1;
 }
--- hahaton/global.h
+++ hahaton/global.h
@@ -4,6 +4,7 @@
 #include <vector>
 #include <windows.system.h>
 #include <ctime>
+#define N 4
 
 struct cam
 {
@@ -14,8 +15,14 @@ struct cam
 
 struct car
 {
-    cam* camera = nullptr;
+    std::vector <cam> camera;
     std::string grz;
+    //int directions[2]; // directions[0] - начальный сектор, directions[1] - конечный сектор, directions[0] == directions[1] == -1 => ошибка => в этом случае не учитываем машину
+};
+
+struct area
+{
+    double coords[2][2];
 };
 
 class global
@@ -24,9 +31,14 @@ private:
     bool check(double[2], double[2]);
 
 public:
+
     double left_down[2], right_up[2];
 
+    int matrix_directions[N][N] = { {0} };
+
     car* cars = nullptr;
+
+    area* areas = nullptr;
 
     pqxx::connection *Connection;
 
@@ -38,5 +50,12 @@ public:
 
     void calc_corners();
 
-    void take_cars(std::string, std::string, double[2], double[2]);
+    void take_cars(std::string start_time, std::string end_time, double* begin_pos, double* end_pos);
+
+
+    area* make_areas();
+
+    int* take_direction(cam, cam);
+
+    void fill_matrix(int*, int[][N]);
 };
